e132a245dc1f5a413399bb08a86bac772f8df9d6
FoxMoss <foxmoss@mediaology.com>
2025-06-24 12:35:07 -0500

    feat: window closing

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <X11/X.h>
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <X11/keysym.h>
 #include <cstring>
 #include <stdbool.h>
@@ -23,20 +25,21 @@ int x11_warn(Display *display, XErrorEvent *event) {
   return 0;
 }
 
-static Display *root_display = NULL;
-
 typedef struct FrameData {
   Window child;
 } FrameData;
 
+static Display *root_display = NULL;
 std::unordered_map<Window, FrameData> frame_info;
 std::unordered_map<Window, Window> frame_map;
+static Atom window_focus;
 
 static Window root_window;
 void x11_create_display() {
   root_display = XOpenDisplay(NULL);
   root_window = DefaultRootWindow(root_display);
   fatal_unwrap(root_display == NULL, "Cannot open X11 display");
+  window_focus = XInternAtom(root_display, "_NET_ACTIVE_WINDOW", False);
 }
 int fail_wm_detected(Display *display, XErrorEvent *event) {
   fatal_unwrap(true, "Another X11 WM was detected");
@@ -48,20 +51,66 @@ void x11_connect_display() {
                SubstructureRedirectMask | SubstructureNotifyMask);
   XSync(root_display, false);
   XSetErrorHandler(&x11_warn);
+
+  XGrabServer(root_display);
+  Window returned_root, returned_parent;
+  Window *top_level_windows;
+  unsigned int num_top_level_windows;
+  XQueryTree(root_display, root_window, &returned_root, &returned_parent,
+             &top_level_windows, &num_top_level_windows);
+  for (unsigned int i = 0; i < num_top_level_windows; ++i) {
+    frame(top_level_windows[i], true);
+  }
+  XFree(top_level_windows);
+  XUngrabServer(root_display);
+}
+
+void x11_focus(Window win) {
+  XSetInputFocus(root_display, win, RevertToPointerRoot, CurrentTime);
+  XChangeProperty(root_display, root_window, window_focus, XA_WINDOW, 32,
+                  PropModeReplace, (unsigned char *)&(win), 1);
 }
 void x11_handle_key(const XKeyEvent e) {
   if (frame_map.find(e.window) == frame_map.end())
     return;
-  // not my window not my problem
-  XDestroyWindow(root_display, e.window);
+  XKillClient(root_display, e.window);
+  x11_focus(root_window);
 }
-void x11_handle_map(const XMapRequestEvent e) {
+
+void frame(Window window, bool old) {
   const unsigned int BORDER_WIDTH = 3;
   const unsigned long BORDER_COLOR = 0xff0000;
   const unsigned long BG_COLOR = 0x0000ff;
 
   XWindowAttributes x_window_attrs;
-  XGetWindowAttributes(root_display, e.window, &x_window_attrs);
+  XGetWindowAttributes(root_display, window, &x_window_attrs);
+
+  if (old) {
+    if (x_window_attrs.override_redirect ||
+        x_window_attrs.map_state != IsViewable) {
+      return;
+    }
+  }
+
+  const Window frame = XCreateSimpleWindow(
+      root_display, root_window, x_window_attrs.x, x_window_attrs.y,
+      x_window_attrs.width, x_window_attrs.height, BORDER_WIDTH, BORDER_COLOR,
+      BG_COLOR);
+  XSelectInput(root_display, frame,
+               SubstructureRedirectMask | SubstructureNotifyMask);
+  XAddToSaveSet(root_display, window);
+  XReparentWindow(root_display, window, frame, 0, 0);
+  XMapWindow(root_display, frame);
+  XMapWindow(root_display, window);
+
+  XGrabKey(root_display, XKeysymToKeycode(root_display, XK_KP_Add), AnyModifier,
+           window, false, GrabModeAsync, GrabModeSync);
+
+  frame_info[frame] = {window};
+  frame_map[window] = frame;
+  x11_focus(window);
+}
+void x11_handle_map(const XMapRequestEvent e) {
 
   char *name;
   XFetchName(root_display, e.window, &name);
@@ -69,7 +118,6 @@ void x11_handle_map(const XMapRequestEvent e) {
     XWindowAttributes root_window_attrs;
     XGetWindowAttributes(root_display, root_window, &root_window_attrs);
 
-    XMapWindow(root_display, e.window);
     XWindowChanges changes;
     changes.x = 0;
     changes.y = 0;
@@ -79,27 +127,12 @@ void x11_handle_map(const XMapRequestEvent e) {
 
     XConfigureWindow(root_display, e.window,
                      CWX | CWY | CWWidth | CWHeight | CWStackMode, &changes);
+    XMapWindow(root_display, e.window);
 
     return;
   }
 
-  const Window frame = XCreateSimpleWindow(
-      root_display, root_window, x_window_attrs.x, x_window_attrs.y,
-      x_window_attrs.width, x_window_attrs.height, BORDER_WIDTH, BORDER_COLOR,
-      BG_COLOR);
-  XSelectInput(root_display, frame,
-               SubstructureRedirectMask | SubstructureNotifyMask |
-                   KeyPressMask);
-  XAddToSaveSet(root_display, e.window);
-  XReparentWindow(root_display, e.window, frame, 0, 0);
-  XMapWindow(root_display, frame);
-  XMapWindow(root_display, e.window);
-
-  XGrabKey(root_display, XKeysymToKeycode(root_display, XK_F4), Mod1Mask,
-           e.window, false, GrabModeSync, GrabModeSync);
-
-  frame_info[frame] = {e.window};
-  frame_map[e.window] = frame;
+  frame(e.window, false);
 }
 
 void x11_handle_destroy_window(const XDestroyWindowEvent e) {
@@ -152,8 +185,8 @@ int main(int argc, char **argv) {
       break;
     }
     case KeyPress: {
+      printf("key pressed!\n");
       x11_handle_key(e.xkey);
-
       break;
     }
     default:
--- src/main.h
+++ src/main.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#include <X11/X.h>
+void frame(Window window, bool old);
 void cleanup();
