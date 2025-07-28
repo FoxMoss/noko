f2da47bdc4b09c97ee61cf83081989da385a9fa0
FoxMoss <foxmoss@mediaology.com>
2025-07-28 17:58:34 -0500

    feat: dynamic app finding

--- desktop/homescreen.cc
+++ desktop/homescreen.cc
@@ -4,12 +4,14 @@
 #include "sdl_clay.h"
 #include "trie.h"
 #include "utils.h"
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctype.h>
+#include <math.h>
 #include <stddef.h>
 
 char char_to_t9(char c) {
@@ -68,32 +70,30 @@ void to_t9(const char *word, char **output) {
 HomeScreen::HomeScreen(ProgState *state,
                        void (*display_app_callback)(const char *))
     : display_app_callback(display_app_callback) {
-  App search = {"Search",
-                std::optional(IMG_LoadTexture(state->renderer,
-                                              PUBLIC_FOLDER "search_app.png")),
-                App::APP_INTERNAL,
-                {.internal_callbacks = {&HomeScreen::search_render,
-                                        &HomeScreen::search_key_event}}};
+  App search("Search",
+             std::optional(IMG_LoadTexture(state->renderer,
+                                           PUBLIC_FOLDER "search_app.png")),
+             {&HomeScreen::search_render, &HomeScreen::search_key_event});
   apps.push_back(search);
-  App editor = {"Editor",
-                std::optional(IMG_LoadTexture(state->renderer, PUBLIC_FOLDER
-                                              "placeholder_app.png")),
-                App::APP_EXTERNAL,
-                {.external_path = "/usr/bin/gedit"}};
+  App editor("Editor",
+             std::optional(IMG_LoadTexture(state->renderer, PUBLIC_FOLDER
+                                           "placeholder_app.png")),
+             "/usr/bin/gedit");
   apps.push_back(editor);
-  App terminal = {"Terminal",
-                  std::optional(IMG_LoadTexture(state->renderer, PUBLIC_FOLDER
-                                                "placeholder_app.png")),
-                  App::APP_EXTERNAL,
-                  {.external_path = "/usr/bin/xterm"}};
+  App terminal("Terminal",
+               std::optional(IMG_LoadTexture(state->renderer, PUBLIC_FOLDER
+                                             "placeholder_app.png")),
+               "/usr/bin/xterm");
   apps.push_back(terminal);
+
+  read_desktop_apps_in(state, "/usr/share/applications/");
 
   app_name_root = trie_create_root();
   for (auto app : apps) {
-    char *output = (char *)SDL_malloc(strlen(app.name));
-    to_t9(app.name, &output);
+    char *output = (char *)SDL_malloc(strlen(app.name.c_str()));
+    to_t9(app.name.c_str(), &output);
     TrieNode *node = trie_fillout_path(app_name_root, output);
-    trie_appened_child(node, trie_create_word({(char *)app.name, 1}));
+    trie_appened_child(node, trie_create_word({(char *)app.name_str, 1}));
     free(output);
   }
   search_text[0] = 0;
@@ -102,12 +102,17 @@ HomeScreen::HomeScreen(ProgState *state,
       IMG_LoadTexture(state->renderer, PUBLIC_FOLDER "blank_app.png");
 }
 
-std::optional<App> HomeScreen::get_first_child(HomeScreen *self,
-                                               TrieNode *node) {
+std::optional<App> HomeScreen::get_n_child(HomeScreen *self, TrieNode *node,
+                                           size_t *index, size_t search_index) {
   if (node->type == TRIE_WORD) {
-    App selected_app;
+    if (*index != search_index) {
+      (*index)++;
+      return {};
+    }
+
+    std::optional<App> selected_app;
     for (auto app : self->apps) {
-      if (strcmp(app.name, node->data.w.str) == 0) {
+      if (app.name_str == node->data.w.str) {
         selected_app = app;
         break;
       }
@@ -115,7 +120,7 @@ std::optional<App> HomeScreen::get_first_child(HomeScreen *self,
     return selected_app;
   }
   for (size_t i = 0; i < node->children_len; i++) {
-    auto val = get_first_child(self, node->children[i]);
+    auto val = get_n_child(self, node->children[i], index, search_index);
     if (val.has_value())
       return val;
   }
@@ -155,10 +160,22 @@ void HomeScreen::search_key_event(HomeScreen *self, SDL_Event *event) {
   }
 
   if (new_char != 0) {
+    self->search_index = 0;
     self->search_text[self->search_cursor] = new_char;
     if (self->search_cursor < 255)
       self->search_cursor += 1;
     self->search_text[self->search_cursor] = 0;
+  }
+
+  if (event->key.key == SDLK_KP_MULTIPLY || event->key.key == SDLK_DOWN) {
+    self->search_index++;
+    self->search_index %= self->search_apps_length;
+  }
+  if (event->key.key == SDLK_UP) {
+    if (self->search_index == 0)
+      self->search_index = self->search_apps_length - 1;
+    else
+      self->search_index--;
   }
 
   if (event->key.key == SDLK_KP_MINUS || event->key.key == SDLK_MINUS) {
@@ -166,9 +183,11 @@ void HomeScreen::search_key_event(HomeScreen *self, SDL_Event *event) {
       self->search_cursor--;
     self->search_text[self->search_cursor] = 0;
   }
-  if (event->key.key == SDLK_KP_ENTER) {
+  if (event->key.key == SDLK_KP_ENTER || event->key.key == SDLK_RETURN ||
+      event->key.key == SDLK_RIGHT) {
     TrieNode *node = trie_fillout_path(self->app_name_root, self->search_text);
-    auto val = get_first_child(self, node);
+    size_t index = 0;
+    auto val = self->get_n_child(self, node, &index, self->search_index);
     if (val.has_value()) {
       self->subrender = {};
       self->subkey_event = {};
@@ -176,19 +195,18 @@ void HomeScreen::search_key_event(HomeScreen *self, SDL_Event *event) {
       self->search_text[0] = 0;
       switch (val.value().type) {
       case App::APP_INTERNAL:
-        self->subrender = val.value().data.internal_callbacks.render;
-        self->subkey_event = val.value().data.internal_callbacks.key_event;
+        self->subrender = val.value().internal_callbacks.render;
+        self->subkey_event = val.value().internal_callbacks.key_event;
         break;
       case App::APP_EXTERNAL:
         if (!self->display_app_callback.has_value())
           break;
-        self->display_app_callback.value()(val.value().data.external_path);
+        self->display_app_callback.value()(val.value().external_path.c_str());
       }
     }
   }
 }
 void HomeScreen::search_render(HomeScreen *self, ProgState *state) {
-
   auto layout = HomeScreen::search_layout(self, state, self->search_text);
   SDL_Clay_RenderClayCommandsProxy(&state->render_data, &layout);
 }
@@ -198,7 +216,7 @@ void HomeScreen::key_event(SDL_Event *event) {
 
   if (subkey_event.has_value()) {
     subkey_event.value()(this, event);
-    if (event->key.key != SDLK_KP_PLUS || SDLK_PLUS)
+    if (event->key.key != SDLK_KP_PLUS && event->key.key != SDLK_EQUALS)
       return;
     subkey_event = {};
     subrender = {};
@@ -244,13 +262,13 @@ void HomeScreen::key_event(SDL_Event *event) {
 
   switch (apps[real_i].type) {
   case App::APP_INTERNAL:
-    subrender = apps[real_i].data.internal_callbacks.render;
-    subkey_event = apps[real_i].data.internal_callbacks.key_event;
+    subrender = apps[real_i].internal_callbacks.render;
+    subkey_event = apps[real_i].internal_callbacks.key_event;
     break;
   case App::APP_EXTERNAL:
     if (!display_app_callback.has_value())
       break;
-    display_app_callback.value()(apps[real_i].data.external_path);
+    display_app_callback.value()(apps[real_i].external_path.c_str());
   }
 }
 
@@ -267,34 +285,37 @@ void HomeScreen::render(ProgState *state) {
 void HomeScreen::traverse_apps(HomeScreen *self, ProgState *state,
                                TrieNode *node, size_t *index) {
   if (node->type == TRIE_WORD) {
-    App selected_app;
+    std::optional<App> selected_app;
     for (auto app : self->apps) {
-      if (strcmp(app.name, node->data.w.str) == 0) {
+      if (app.name_str == node->data.w.str) { // uses same pointer
         selected_app = app;
         break;
       }
     }
+    if (!selected_app.has_value())
+      return;
+
     float font_size = (float)state->height / 25;
+    float row_height = (float)state->height / 7;
 
     CLAY({.id = CLAY_IDI("App", *index),
           .layout =
               {
                   .sizing = {.width = CLAY_SIZING_GROW(0),
-                             .height = CLAY_SIZING_FIT(0)},
-                  .padding = CLAY_PADDING_ALL(
-                      (uint16_t)min((float)state->width / 80, 16)),
+                             .height = CLAY_SIZING_FIXED(row_height)},
                   .childGap = 30,
                   .childAlignment = {.x = CLAY_ALIGN_X_LEFT,
                                      .y = CLAY_ALIGN_Y_CENTER},
                   .layoutDirection = CLAY_LEFT_TO_RIGHT,
 
               },
-          .backgroundColor = {255, 255, 255, (float)(*index == 0 ? 50 : 0)},
+          .backgroundColor = {255, 255, 255,
+                              (float)(*index == self->search_index ? 50 : 0)},
           .cornerRadius = CLAY_CORNER_RADIUS(10)}) {
       Clay_String name =
           Clay_String{.isStaticallyAllocated = true,
-                      .length = (int32_t)strlen(selected_app.name),
-                      .chars = selected_app.name};
+                      .length = (int32_t)selected_app->name.size(),
+                      .chars = selected_app->name_str};
       CLAY({.id = CLAY_IDI("AppImage", *index),
             .layout =
                 {
@@ -303,8 +324,8 @@ void HomeScreen::traverse_apps(HomeScreen *self, ProgState *state,
                 },
             .aspectRatio = {1},
             .image = {
-                .imageData = selected_app.icon.has_value()
-                                 ? selected_app.icon.value()
+                .imageData = selected_app->icon.has_value()
+                                 ? selected_app->icon.value()
                                  : self->blank_app_texture,
             }});
       CLAY_TEXT(name,
@@ -315,6 +336,7 @@ void HomeScreen::traverse_apps(HomeScreen *self, ProgState *state,
       *index = *index + 1;
     }
   }
+
   for (size_t i = 0; i < node->children_len; i++) {
     traverse_apps(self, state, node->children[i], index);
   }
@@ -328,6 +350,7 @@ Clay_RenderCommandArray HomeScreen::search_layout(HomeScreen *self,
   Clay_Sizing layoutExpand = {.width = {(float)state->width},
                               .height = {(float)state->height}};
   float font_size = (float)state->height / 25;
+  float row_height = (float)state->height / 7;
   size_t index = 0;
   CLAY({.id = CLAY_ID("OuterContainer"),
         .layout = {
@@ -342,10 +365,9 @@ Clay_RenderCommandArray HomeScreen::search_layout(HomeScreen *self,
             {
                 .sizing = {.width = CLAY_SIZING_FIXED(
                                state->width - (float)state->height / 20),
-                           .height = CLAY_SIZING_FIXED(
-                               state->height - (float)state->height / 20 -
-                               (float)state->height / 14)},
+                           .height = CLAY_SIZING_GROW(0)},
                 .padding = CLAY_PADDING_ALL(10),
+                .childGap = 10,
                 .layoutDirection = CLAY_TOP_TO_BOTTOM,
             },
         .backgroundColor = {0, 0, 0, 100},
@@ -369,18 +391,23 @@ Clay_RenderCommandArray HomeScreen::search_layout(HomeScreen *self,
       TrieNode *node =
           trie_fillout_path(self->app_name_root, self->search_text);
       size_t index = 0;
-      CLAY({.id = CLAY_ID("AppList"),
-            .layout = {
-                .sizing = {.width = CLAY_SIZING_GROW(0),
-                           .height = CLAY_SIZING_FIT(0)},
-                .padding = CLAY_PADDING_ALL(10),
-                .childGap = 10,
-                .childAlignment = {.x = CLAY_ALIGN_X_LEFT,
-                                   .y = CLAY_ALIGN_Y_TOP},
-                .layoutDirection = CLAY_TOP_TO_BOTTOM,
-            }}) {
+      float offset = fmin((-row_height * self->search_index) + 20, 0);
+      CLAY({
+          .id = CLAY_ID("AppList"),
+          .layout =
+              {
+                  .sizing = {.width = CLAY_SIZING_GROW(0),
+                             .height = CLAY_SIZING_FIT(0)},
+                  .padding = CLAY_PADDING_ALL(10),
+                  .childAlignment = {.x = CLAY_ALIGN_X_LEFT,
+                                     .y = CLAY_ALIGN_Y_TOP},
+                  .layoutDirection = CLAY_TOP_TO_BOTTOM,
+              },
+          .clip = {.vertical = true, .childOffset = {.y = offset}},
+      }) {
 
         traverse_apps(self, state, node, &index);
+        self->search_apps_length = index;
       }
     }
   }
@@ -444,11 +471,14 @@ Clay_RenderCommandArray HomeScreen::generate_layout(ProgState *state) {
   Clay_Sizing layoutExpand = {.width = {(float)state->width},
                               .height = {(float)state->height}};
   float max_strlen = 0;
-  float font_size = (float)state->height / 25;
-  for (auto app : apps) {
+  float font_size = (float)state->height / 30;
+  for (int i = 0; i < 9; i++) {
+    App app = apps[i];
     int width, height;
+
     TTF_SetFontSize(state->lato_regular, font_size);
-    if (!TTF_GetStringSize(state->lato_regular, app.name, 0, &width, &height)) {
+    if (!TTF_GetStringSize(state->lato_regular, app.name.c_str(), 0, &width,
+                           &height)) {
       SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to measure text: %s",
                    SDL_GetError());
     }
@@ -490,24 +520,23 @@ Clay_RenderCommandArray HomeScreen::generate_layout(ProgState *state) {
         }) {
           for (size_t col = 0; col < 3; col++) {
 
-            CLAY(
-                {.id = CLAY_IDI("App", index),
-                 .layout = {
-                     .sizing = {.width = CLAY_SIZING_FIXED((float)(max_strlen)),
-                                .height = CLAY_SIZING_GROW(0)},
-                     .padding = CLAY_PADDING_ALL(
-                         (uint16_t)min((float)state->width / 40, 16)),
-                     .childAlignment = {.x = CLAY_ALIGN_X_CENTER,
-                                        .y = CLAY_ALIGN_Y_CENTER},
-                     .layoutDirection = CLAY_TOP_TO_BOTTOM,
-                 }}) {
+            CLAY({.id = CLAY_IDI("App", index),
+                  .layout = {
+                      .sizing = {.width =
+                                     CLAY_SIZING_FIXED((float)(max_strlen) + 8),
+                                 .height = CLAY_SIZING_GROW(0)},
+                      .childAlignment = {.x = CLAY_ALIGN_X_CENTER,
+                                         .y = CLAY_ALIGN_Y_CENTER},
+                      .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                  }}) {
               if (index < apps.size()) {
                 App app = apps[index];
 
                 Clay_String name =
                     Clay_String{.isStaticallyAllocated = true,
-                                .length = (int32_t)strlen(app.name),
-                                .chars = app.name};
+                                .length = (int32_t)app.name.size(),
+                                .chars = app.name_str};
+
                 CLAY({.id = CLAY_IDI("AppImage", index),
                       .layout = {.sizing = {.width = {(float)state->height / 7},
                                             .height = {(float)state->height /
--- desktop/homescreen.h
+++ desktop/homescreen.h
@@ -1,29 +1,52 @@
 #pragma once
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_render.h"
+#include "inipp.h"
 #include "main.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <string>
 extern "C" {
 #include "trie.h"
 }
 #include <SDL3_image/SDL_image.h>
 #include <cstddef>
+#include <iostream>
 #include <optional>
 #include <vector>
 
-struct App {
-  const char *name;
+struct AppCallbacks {
+  void (*render)(HomeScreen *self, ProgState *);
+  void (*key_event)(HomeScreen *self, SDL_Event *);
+};
+
+class App {
+public:
+  App(std::string name, std::optional<SDL_Texture *> icon,
+      AppCallbacks callbacks)
+      : name(name), icon(icon), type(APP_INTERNAL),
+        internal_callbacks(callbacks) {
+    name_str = (char *)malloc(name.size() + 1);
+    memcpy(name_str, name.data(), name.size());
+  }
+  App(std::string name, std::optional<SDL_Texture *> icon, std::string path)
+      : name(name), icon(icon), type(APP_EXTERNAL), external_path(path) {
+    name_str = (char *)malloc(name.size());
+    memcpy(name_str, name.data(), name.size());
+  }
+  ~App() {}
+
+  std::string name;
+  char *name_str;
   std::optional<SDL_Texture *> icon;
 
   enum AppType { APP_INTERNAL, APP_EXTERNAL } type;
-  union AppDataUnion {
-    struct {
-      void (*render)(HomeScreen *self, ProgState *);
-      void (*key_event)(HomeScreen *self, SDL_Event *);
+  AppCallbacks internal_callbacks;
 
-    } internal_callbacks;
-
-    const char *external_path;
-  } data;
+  std::string external_path;
 };
 
 class HomeScreen {
@@ -37,6 +60,91 @@ public:
   static Clay_RenderCommandArray app_demo(ProgState *state, char *name);
 
 private:
+  std::optional<std::string> find_executable(std::string executable_name) {
+    for (auto bin_dir : {"/usr/bin/", "/bin/", "/usr/local/bin/"}) {
+      if (!std::filesystem::exists(bin_dir))
+        continue;
+
+      for (auto file : std::filesystem::directory_iterator(bin_dir)) {
+        if (!file.is_regular_file())
+          continue;
+        if (file.path().filename() != executable_name)
+          continue;
+        return file.path().string();
+      }
+    }
+    return {};
+  }
+
+  std::optional<std::string> find_icon(std::string icon_name) {
+    for (auto icon_dir : {"/usr/share/icons/hicolor/256x256/apps/",
+                          "/usr/share/icons/hicolor/scalable/apps/"}) {
+
+      if (!std::filesystem::exists(icon_dir))
+        continue;
+
+      for (auto file : std::filesystem::directory_iterator(icon_dir)) {
+        if (!file.is_regular_file())
+          continue;
+        if (file.path().stem() != icon_name)
+          continue;
+        return file.path().string();
+      }
+    }
+    return {};
+  }
+
+  void read_desktop_apps_in(ProgState *state, std::string dir) {
+    if (!std::filesystem::exists(dir))
+      return;
+    for (auto file : std::filesystem::directory_iterator(dir.c_str())) {
+      if (!file.is_regular_file())
+        continue;
+      if (file.path().extension() != ".desktop")
+        continue;
+
+      inipp::Ini<char> ini;
+      std::ifstream is(file.path());
+      ini.parse(is);
+      if (!ini.sections.contains("Desktop Entry"))
+        continue;
+
+      std::string executable_name;
+      if (!inipp::get_value(ini.sections["Desktop Entry"], "Exec",
+                            executable_name))
+        continue;
+      if (executable_name.size() < 2)
+        continue;
+      std::string name;
+      if (!inipp::get_value(ini.sections["Desktop Entry"], "Name", name))
+        continue;
+      std::string icon_name;
+      if (!inipp::get_value(ini.sections["Desktop Entry"], "Icon", icon_name))
+        continue;
+
+      auto icon_file_op = find_icon(icon_name);
+      if (!icon_file_op.has_value())
+        continue;
+
+      std::string icon_file = icon_file_op.value();
+
+      std::string executable_path;
+      if (executable_name[0] == '/') // prob a path
+        executable_path = executable_name;
+      else {
+        auto executable_path_op = find_executable(executable_name);
+        if (!executable_path_op.has_value())
+          continue;
+        executable_path = executable_path_op.value();
+      }
+
+      App app(
+          name,
+          std::optional(IMG_LoadTexture(state->renderer, icon_file.c_str())),
+          executable_path);
+      apps.push_back(app);
+    }
+  }
   std::optional<void (*)(HomeScreen *self, ProgState *state)> subrender = {};
   std::optional<void (*)(HomeScreen *self, SDL_Event *event)> subkey_event = {};
   std::optional<void (*)(const char *name)> display_app_callback;
@@ -46,13 +154,16 @@ private:
 
   char search_text[256];
   size_t search_cursor = 0;
+  size_t search_index = 0;
+  size_t search_apps_length = 0;
   static void traverse_apps(HomeScreen *self, ProgState *state, TrieNode *node,
                             size_t *index);
   static Clay_RenderCommandArray
   search_layout(HomeScreen *self, ProgState *state, const char *text);
   static void search_render(HomeScreen *self, ProgState *state);
-  static std::optional<App> get_first_child(HomeScreen *self, TrieNode *node);
   static void search_key_event(HomeScreen *self, SDL_Event *event);
+  std::optional<App> get_n_child(HomeScreen *self, TrieNode *node,
+                                 size_t *index, size_t search_index);
 
   TrieNode *app_name_root;
 };
